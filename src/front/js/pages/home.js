41c04984ee949eab269a6f835a71e80aaccd1bf2
OVYED Deno <nevad34@gmail.com>
2024-09-29 20:20:40 +0000

    9/29

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,24 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import React from 'react';
 import "../../styles/home.css";
 
-export const Home = () => {
-	const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+export const Home = () => {
+  return (
+    <div className="container">
+      <h1 className="selena-title">S E L E N A</h1>
+
+      <div className="center-content">
+        <h2 className="find-your">FIND YOUR</h2>
+        <h2 className="muse">MUSE</h2>
+
+        <button className="enter-button">Enter
+          <img 
+            src="https://hydrapolis.us/wp-content/uploads/2022/12/PLAYA-BLANCA-986x1024.png" 
+            alt="Beach" 
+            className="right-image"
+          /></button>
+      </div>
+    </div>
+
+  );
 };
