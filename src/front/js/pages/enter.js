5382717a1ac4ca5490d63b8889e73927f9599e3d
OVYED Deno <nevad34@gmail.com>
2024-09-30 16:32:51 +0000

    9/30

--- src/front/js/pages/enter.js
+++ src/front/js/pages/enter.js
@@ -6,28 +6,26 @@ export const Enter = () => {
   return (
     <div className="container">
       <h1 className="selena-title">S E L E N A</h1>
-        <div className="input-group">
-          <label htmlFor="name" className="input-label">
-            Name
-          </label>
-          <input type="text" id="name" className="input-field" />
-        </div>
-        <div className="input-group">
-          <label htmlFor="roomPin" className="input-label">
-            Room Pin
-          </label>
-          <input type="text" id="roomPin" className="input-field" />
-        </div>
-        <Link to="/room">
-            <button className="enter-button">
-              Enter
-              <img
-                src="https://hydrapolis.us/wp-content/uploads/2022/12/PLAYA-BLANCA-986x1024.png"
-                alt="PLAYA-BLANCA"
-                className="right-image"
-              />
-            </button>
-          </Link>
-    </div>
+      <div className="group-input">
+      <label htmlFor="name" className="input-label">
+        Name
+      </label>
+      <input type="text" id="name" className="input-field" />
+      <label htmlFor="roomPin" className="input-label">
+        Room Pin
+      </label>
+      <input type="text" id="roomPin" className="input-field" />
+      </div>
+      <Link to="/room">
+        <button className="enter-button">
+          Enter
+          <img
+            src="https://hydrapolis.us/wp-content/uploads/2022/12/PLAYA-BLANCA-986x1024.png"
+            alt="PLAYA-BLANCA"
+            className="right-image"
+          />
+        </button>
+      </Link>
+     </div>
   );
 };
