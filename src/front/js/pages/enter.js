b720d965840826fafce220f4d5ee4cd648903bfc
OVYED Deno <nevad34@gmail.com>
2024-10-15 16:05:51 +0000

    10/15

--- src/front/js/pages/enter.js
+++ src/front/js/pages/enter.js
@@ -16,6 +16,7 @@ export const Enter = () => {
       </label>
       <input type="text" id="roomPin" className="input-field" />
       </div>
+      <div className="button-container">
       <Link to="/room">
         <button className="enter-button">
           Enter
@@ -26,6 +27,16 @@ export const Enter = () => {
           />
         </button>
       </Link>
-     </div>
+      <Link to="/shop">
+        <button className="enter-button">
+          Auth 
+          <img
+            src="https://hydrapolis.us/wp-content/uploads/2022/12/PLAYA-BLANCA-986x1024.png"
+            alt="PLAYA-BLANCA"
+            className="right-image"
+          />
+        </button>
+      </Link>
+     </div></div>
   );
 };
